de848e915b178c0e547aabf499f5cb7eb055f00c
sophda <1309707832@qq.com>
2023-11-01 19:49:58 +0800

    first add

---
+++ test/build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/fun.cpp.o"
+  "CMakeFiles/test.dir/fun.cpp.o.d"
+  "libtest.pdb"
+  "libtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
