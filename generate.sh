6d2a2a4434ed26a6ec868dfe05054283983cf9db
sophda <1309707832@qq.com>
2024-01-27 15:57:00 +0800

    well track on different devices

--- generate.sh
+++ generate.sh
@@ -2,4 +2,4 @@ cd /home/sophda/project/OrbSlam3AR/arm64_build
 rm -rf ./*
 cmake-gui -S /home/sophda/project/OrbSlam3AR -B /home/sophda/project/OrbSlam3AR/arm64_build
 make -j64
-cp /home/sophda/project/OrbSlam3AR/ORB_SLAM3/arm64_build/libORB_SLAM3.so /home/sophda/project/OrbSlam3AR/arm64_build/
+# cp /home/sophda/project/OrbSlam3AR/ORB_SLAM3/arm64_build/libORB_SLAM3.so /home/sophda/project/OrbSlam3AR/arm64_build/
